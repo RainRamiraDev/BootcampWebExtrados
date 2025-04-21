49190169aa97364ece60af322df4bda719deff5f
Rain Ramira <114005@tecnicatura.frc.utn.edu.ar>
2025-04-21 17:27:30 -0300

    .

---
+++ FrontEnd/Primera tarea/primera-tarea/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './components/Header';
+
+
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>Este es un proyecto básico usando componentes en React.</p>
+    </>
+  );
+}
+
+export default App;
---
+++ FrontEnd/Primera tarea/primera-tarea/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../styles/Header.css';
+
+function Header() {
+  return (
+    <header>
+      <h1>Bienvenidos a mi Proyecto en React</h1>
+    </header>
+  );
+}
+
+export default Header;
